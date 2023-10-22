61e73cc874fd3a1188a5aa29afe40f6f62026898
Damian <kalbarzd@gmail.com>
2023-10-22 18:59:10 +0200

    Implementacja logowania z tokenem JWT i rejstrecji

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -4,13 +4,16 @@ import 'package:firstproject/profil_view.dart';
 import 'package:firstproject/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  void login(BuildContext context) async {
-    var url = Uri.parse('https://example.com/login'); // Zastąp adresem swojego serwera logowania
+
+  /*
+  Future<void> login(BuildContext context) async {
+    var url = Uri.parse('https://localhost:7286/api/Authentication/login'); // Zastąp adresem swojego serwera logowania
     var body = json.encode({
       'username': emailController.text,
       'password': passwordController.text,
@@ -23,7 +26,11 @@ class LoginPage extends StatelessWidget {
     );
 
     if (response.statusCode == 200) {
-      // Zalogowano pomyślnie, przekieruj na główny ekran
+      // Zalogowano pomyślnie, zapisz token do pamięci podręcznej
+      var token = json.decode(response.body)['token'];
+      await _saveToken(token);
+
+      // Przekieruj na główny ekran
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => ProfilPage()),
@@ -49,6 +56,65 @@ class LoginPage extends StatelessWidget {
       );
     }
   }
+*/
+  Future<void> _saveToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+
+
+  void login(BuildContext context) async {
+    var url = Uri.parse('https://localhost:7286/api/Authentication/login'); // Zastąp adresem swojego serwera logowania
+    var body = json.encode({
+      'email': emailController.text,
+      'password': passwordController.text,
+    });
+
+    try {
+      var response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        // Zalogowano pomyślnie, zapisz token do pamięci podręcznej
+        var token = json.decode(response.body)['token'];
+        await _saveToken(token);
+
+        // Przekieruj na główny ekran
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilPage()),
+        );
+      } else {
+        // Obsługa błędów logowania
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Błąd logowania'),
+              content: Text('Nieudane logowanie. Spróbuj ponownie.'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
 
 
   @override
--- lib/register_view.dart
+++ lib/register_view.dart
@@ -1,15 +1,132 @@
+import 'dart:convert';
+
 import 'package:firstproject/profil_view.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatefulWidget {
   _RegisterPageState createState() => _RegisterPageState();
 }
 
+
+
 class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
+  final _fullNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _userNameController = TextEditingController();
+
+  /*
+  Future<void> registerUser(BuildContext context) async {
+    var url = Uri.parse('https://localhost:7286/api/Authentication/register');
+    try {
+      var response = await http.post(url, body: {
+        "userName": _userNameController.text,
+        "password": _passwordController.text,
+        "confirmPassword": _passwordController.text,
+        "fullName": _fullNameController.text,
+        "email": _emailController.text,
+        "phoneNumber": _phoneController.text
+      });
+
+      if (response.statusCode == 200) {
+        var jsonResponse = json.decode(response.body);
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilPage()),
+        );
+      } else {
+        if (response.statusCode == 400) {
+          // Obsługa błędów 400 Bad Request
+          var errorResponse = json.decode(response.body);
+          String errorMessage = errorResponse['message'];
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Błąd rejestracji"),
+                content: Text(errorMessage),
+                actions: [
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // Inne kody stanu HTTP
+          throw Exception('Failed to register user. Status code: ${response.statusCode}');
+        }
+      }
+    } catch (e) {
+      // Obsługa innych błędów, takich jak problemy z połączeniem
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Błąd"),
+            content: Text("Wystąpił problem podczas połączenia z serwerem."),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+   */
+
+  Future<void> registerUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    var url = Uri.parse('https://localhost:7286/api/Authentication/register');
+    try {
+      var response = await http.post(url, headers: {'Content-Type': 'application/json; charset=UTF-8'},
+          body: jsonEncode({
+        "userName": _userNameController.text,
+        "password": _passwordController.text,
+        "confirmPassword": _passwordController.text,
+        "fullName": _fullNameController.text,
+        "email": _emailController.text,
+        "phoneNumber": _phoneController.text
+      }));
+
+      if (response.statusCode == 200) {
+        // Rejestracja zakończona sukcesem
+        // Możesz tutaj dodać odpowiednią obsługę sukcesu, na przykład przenieść użytkownika do innej strony
+        print('Pomyślnie zarejestrowano użytkownika');
+      } else {
+        // Obsługa błędów
+        print('Błąd podczas rejestracji. Kod stanu: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Obsługa innych błędów, takich jak problemy z połączeniem
+      print('Wystąpił błąd: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
 
   bool isChecked = false;
 
@@ -49,20 +166,22 @@ class _RegisterPageState extends State<RegisterPage> {
                       fontFamily: "Bellota-Regular")),
               SizedBox(height: 26.0),
               TextFormField(
+                controller: _userNameController,
                 decoration: InputDecoration(
-                    labelText: 'Imię',
+                    labelText: 'Nazwa użytkownika',
                     labelStyle: TextStyle(fontFamily: "Bellota-Regular")),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Pole "Imię" jest wymagane.';
+                    return 'Pole "Nazwa użytkownika" jest wymagane.';
                   }
                   return null;
                 },
               ),
               SizedBox(height: 16.0),
               TextFormField(
+                controller: _fullNameController,
                 decoration: InputDecoration(
-                    labelText: 'Nazwisko',
+                    labelText: 'Imie i Nazwisko',
                     labelStyle: TextStyle(fontFamily: "Bellota-Regular")),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -105,6 +224,7 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               SizedBox(height: 16.0),
               TextFormField(
+                controller: _passwordController,
                 decoration: InputDecoration(
                     labelText: 'Hasło',
                     labelStyle: TextStyle(fontFamily: "Bellota-Regular")),
@@ -141,10 +261,7 @@ class _RegisterPageState extends State<RegisterPage> {
                 child: ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProfilPage()));
+                      registerUser();
                     }
                   },
                   child: Text('Zarejestruj się',
